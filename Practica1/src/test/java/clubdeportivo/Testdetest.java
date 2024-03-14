9ef1fd96b59bb3677bacc53eccf0b463de252715
antBaena <antbaena@uma.es>
2024-03-14 11:02:57 +0100

    aaaaaaaaa

---
+++ Practica1/src/test/java/clubdeportivo/Testdetest.java
@@ -0,0 +1,17 @@
+package clubdeportivo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Testdetest {
+    @DisplayName("El metodo deposit debe laz una excepcion al introducir una cantidad negativa de dinero")
+    @Test
+    public void Deposit_NegativeAmount_ThrowsException() {
+
+        assertEquals(true, true);
+    }
+}
